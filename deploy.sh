5438ea96847bbf1675ed67d88becc3fe57ef56e1
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2020-11-11 16:21:21 -0500

    deployent changes; ongoing work here

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@
 
 set -e
 
-cargo build --bin=ptrack-server --release
+cargo build --bin=ptrack-server --release --target=x86_64-unknown-linux-gnu
 cargo build --bin=ptrack-agent --release --target=x86_64-pc-windows-gnu
 
-gcloud compute scp target/release/ptrack-server meili-svr:/opt/ptrack/
+gcloud compute scp target/x86_64-unknown-linux-gnu/release/ptrack-server meili-svr:/opt/ptrack/
 gcloud compute scp target/x86_64-pc-windows-gnu/release/ptrack-agent.exe meili-svr:/opt/ptrack/
 gcloud compute ssh meili-svr -- sudo /opt/ptrack/ptrack-server
 
